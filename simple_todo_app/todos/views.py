23ea2fa960da0b2562b27e68b8894c032af657bd
choushen <jacob.mckenzie@icloud.com>
2024-07-15 03:28:07 +0100

    Created url and views for todos app

--- simple_todo_app/todos/views.py
+++ simple_todo_app/todos/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DeleteView
+from .models import Todo
+
 
 # Create your views here.
+def list_todo_items(request):
+    return HttpResponse('Todo list')
+
+# class TodoListView(ListView):
+#     return render(request, 'todos/list_todo_items.html')

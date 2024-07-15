23869ad29a17cfd7afb637db252291dd7e203da8
choushen <jacob.mckenzie@icloud.com>
2024-07-15 01:53:40 +0100

    - Ignoring .env file - Migrating db to postgresql - Added todo model - Added migration file

--- simple_todo_app/simple_todo_app/settings.py
+++ simple_todo_app/simple_todo_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,11 +86,15 @@ WSGI_APPLICATION = 'simple_todo_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, 'postgres_db.env')) # Load the environment variables from the .env file
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
--- simple_todo_app/todos/models.py
+++ simple_todo_app/todos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    createdBy = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+

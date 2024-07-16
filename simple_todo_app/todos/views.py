3b21037abb451e720f2c8ce11f9a03c1a6b19dbc
choushen <jacob.mckenzie@icloud.com>
2024-07-16 02:52:50 +0100

    Added styles, updated views and templates

--- simple_todo_app/todos/views.py
+++ simple_todo_app/todos/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView, DeleteView
 from .models import Todo
+from accounts.models import CustomUser
 
 
 # Create your views here.
+@login_required
 def list_todo_items(request):
-    return render(request, 'todos/todo_dashboard.html')
+    user = CustomUser.objects.get(username=request.user.email)  # Access username correctly
+    todos = Todo.objects.filter(createdBy=user)  # Use filter instead of all
+    return render(request, 'todos/todo_dashboard.html', {'user': user, 'todos': todos})
 
 # class TodoListView(ListView):
 #     return render(request, 'todos/list_todo_items.html')
